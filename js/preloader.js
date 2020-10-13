1ee85cf13c4222ca4b799cab800c2d350857526e
Faisal Jamil <faisaljamilcs@gmail.com>
2020-10-14 00:15:42 +0530

    feat: Preloader added

---
+++ js/preloader.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", () => {
+    setTimeout(() => {
+        const preloader = document.querySelector(".preloader");
+        preloader.classList.add("preloader-finished");
+    }, 1200)
+})
