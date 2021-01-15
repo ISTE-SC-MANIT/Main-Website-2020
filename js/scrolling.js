e6480454d5efafe10374b993bc7aa176d76d0cc6
Faisal Jamil <faisaljamilcs@gmail.com>
2021-01-16 00:11:42 +0530

    feat(sponsors): scrolling

--- js/scrolling.js
+++ js/scrolling.js
@@ -1,14 +1,17 @@
 var navbar = document.getElementById("bar");
 
-window.onscroll = function () { scrollFunction() };
+window.onscroll = function () {
+  scrollFunction();
+};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+  if (
+    document.body.scrollTop > 400 ||
+    document.documentElement.scrollTop > 400
+  ) {
     navbar.classList.add("hhd");
-
   } else {
-    if (navbar.classList.contains("hhd"))
-      navbar.classList.remove("hhd");
+    if (navbar.classList.contains("hhd")) navbar.classList.remove("hhd");
   }
 }
 
@@ -21,12 +24,12 @@ function fadeIn() {
   // Init Controller
   controller = new ScrollMagic.Controller();
   // Select all sections
-  const sections = document.querySelectorAll('.animate-section');
-  const sectionHeading = document.querySelectorAll('.section-heading');
+  const sections = document.querySelectorAll(".animate-section");
+  const sectionHeading = document.querySelectorAll(".section-heading");
   // Loop over each section
-  sections.forEach(section => {
+  sections.forEach((section) => {
     const opacityTl = gsap.timeline({
-      defaults: { duration: 1, ease: "power2.inOut" }
+      defaults: { duration: 1, ease: "power2.inOut" },
     });
     opacityTl.fromTo(section, { opacity: 0 }, { opacity: 1 });
     opacityTl.fromTo(sectionHeading, { y: 20 }, { y: 0 }, "-=1");
@@ -34,12 +37,12 @@ function fadeIn() {
     fadeInScene = new ScrollMagic.Scene({
       triggerElement: section,
       triggerHook: 0.7,
-      reverse: false
+      reverse: false,
     })
       .setTween(opacityTl)
       // .addIndicators({ colorStart: 'red', colorTrigger: 'yellow', name: 'section' })
       .addTo(controller);
-  })
+  });
 }
 
 // Active Nav-link Animation
@@ -47,26 +50,27 @@ function fadeIn() {
 let activeNavScene;
 
 const scenes = {
-  'home': 'home-link',
-  'about-section': 'about-link',
-  'event-section': 'event-link',
-  'contact': 'contact-link'
-}
+  home: "home-link",
+  "about-section": "about-link",
+  "event-section": "event-link",
+  "sponsors-section": "sponsor-link",
+  contact: "contact-link",
+};
 
 function activeNav() {
   // Init Controller
   controller = new ScrollMagic.Controller();
   // Select all sections
-  const sections = document.querySelectorAll('.section');
+  const sections = document.querySelectorAll(".section");
   // Loop over each section
-  sections.forEach(section => {
+  sections.forEach((section) => {
     let link = scenes[section.id];
     if (section.id === "contact") {
       var hook = 0.75;
       var durtn = section.clientHeight - 50;
-    } else if (section.id === "event-section") {
+    } else if (section.id === "sponsors-section") {
       var hook = 0.25;
-      var durtn = section.clientHeight - 150;
+      var durtn = section.clientHeight - 260;
     } else {
       var hook = 0.25;
       var durtn = section.clientHeight + 100;
@@ -75,12 +79,16 @@ function activeNav() {
       triggerElement: section,
       triggerHook: hook,
       duration: durtn,
-      reverse: true
+      reverse: true,
     })
-      // .addIndicators({ colorStart: 'red', colorTrigger: 'yellow', name: 'section' })
-      .setClassToggle('#' + link, 'active')
+      // .addIndicators({
+      //   colorStart: "red",
+      //   colorTrigger: "yellow",
+      //   name: "section",
+      // })
+      .setClassToggle("#" + link, "active")
       .addTo(controller);
-  })
+  });
 }
 
 // goto top Animation
@@ -91,16 +99,16 @@ function gotoTop() {
   // Init Controller
   controller = new ScrollMagic.Controller();
   // Select goto-top btn
-  const gotoTopBtn = document.querySelector('.goto-top');
+  const gotoTopBtn = document.querySelector(".goto-top");
   const fadeTl = gsap.timeline({
-    defaults: { duration: 0.1, ease: "power2.inOut" }
+    defaults: { duration: 0.1, ease: "power2.inOut" },
   });
   fadeTl.fromTo(gotoTopBtn, { opacity: 0 }, { opacity: 1 });
   // Create Scene
   gotoTopScene = new ScrollMagic.Scene({
     triggerElement: ".goto-trigger",
     triggerHook: 0.7,
-    reverse: true
+    reverse: true,
   })
     .setTween(fadeTl)
     .setClassToggle(".goto-top", "show")
